5221b4e6779fcca2887c2e0b54ae9bb0537ba294
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-03-10 14:38:27 -0700

    refactor(client): react native into client folder

---
+++ client/App.js
@@ -0,0 +1,11 @@
+// Native Imports
+import React from 'react';
+
+// Navigation
+import FresnoFreshNavigator from './src/components/navigation/FresnoFreshNavigator'
+
+export default function App() {
+  return (
+    <FresnoFreshNavigator />
+  );
+}
