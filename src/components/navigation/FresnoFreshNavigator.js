b0f9d2f3197ddbcc624d08d71b082ddfcc0e74b1
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-03-10 14:21:31 -0700

    feat(navigation): StackNavigator setup

---
+++ src/components/navigation/FresnoFreshNavigator.js
@@ -0,0 +1,38 @@
+// Native Imports
+import React from 'react'
+
+// React Navigation
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+
+// Screens
+import HomeScreen from '../screens/HomeScreen'
+import ListingsScreen from '../screens/ListingsScreen'
+
+// Navigation Options
+const navigationOptions = (title) => {
+    return {
+        headerTitle: title,
+        headerStyle: {
+            backgroundColor: '#2c6ab5',
+            
+        },
+        headerTitleStyle: {
+            color: 'white',
+            fontSize: 26
+        },
+    }
+}
+
+// Stack Navigation
+const StackNavigation = createStackNavigator(
+    {
+        Home: HomeScreen,
+        Listings: ListingsScreen
+    },
+    {
+        defaultNavigationOptions: navigationOptions('Fresno Fresh')
+    }
+)
+
+export default createAppContainer(StackNavigation);
