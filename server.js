30ddec433d73e1c45ac95cc2c217cfee36cbce02
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-03-11 13:49:08 -0700

    feat(server.js): Configured Server

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const users = require('./routes/users');
 
 const app = express();
-const PORT = process.env.PORT || 8000
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
+// Body Parser Middleware
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+// DB Config
+const DB = require('./config/keys').mongoURI
+
+// Connect to Mongo
+mongoose
+    .connect(DB)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/user', users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
