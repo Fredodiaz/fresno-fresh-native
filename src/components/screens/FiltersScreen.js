b272ff97950d5f69e4870f4d8dfbbc0203bafd9c
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-03-12 10:51:27 -0700

    feat(FiltersScreen): FiltersScreen BoilerPlate

---
+++ src/components/screens/FiltersScreen.js
@@ -0,0 +1,71 @@
+// Native Imports
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, Switch, Button } from 'react-native'
+
+const FiltersScreen = () => {
+    const [ isGlutenFree, setIsGlutenFree ] = useState(false)
+    const [ isLactoseFree, setIsLactoseFree ] = useState(false)
+    const [ isVegan, setIsVegan ] = useState(false)
+    const [ isVegetarian, setIsVegetarian ] = useState(false)
+
+    const renderSwitch = (func, val) => {
+        return (
+            <Switch 
+            trackColor={{true: '#2c6ab5'}}
+            thumbColor={{true: '#2c6ab5'}}
+            value={val}
+            onValueChange={newVal => func(newVal)}
+            />
+        )
+    }
+
+    return (
+        <View style={styles.screen}>
+            <Text style={styles.alignedTitle}>Available Filters / Restricitons</Text>
+            <View style={styles.filterContainer}>
+                <Text style={styles.filterText}>Gluten-free</Text>
+                {renderSwitch(setIsGlutenFree, isGlutenFree)}
+            </View>
+            <View style={styles.filterContainer}>
+                <Text style={styles.filterText}>Lactose-free</Text>
+                {renderSwitch(setIsLactoseFree, isLactoseFree)}
+            </View>
+            <View style={styles.filterContainer}>
+                <Text style={styles.filterText}>Vegan</Text>
+                {renderSwitch(setIsVegan, isVegan)}
+            </View>
+            <View style={styles.filterContainer}>
+                <Text style={styles.filterText}>Vegetarian</Text>
+                {renderSwitch(setIsVegetarian, isVegetarian)}
+            </View>
+            <View style={{marginTop: 30}}></View>
+            <Button onPress={() => alert("Preferences Saved")} title="Save Filters"/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    alignedTitle: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginTop: 15,
+        marginBottom: 20,
+    },
+    filterContainer: {
+        width: 270,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 10
+    },
+    filterText: {
+        fontSize: 18,
+    }
+})
+
+export default FiltersScreen
