b046e1e6c287efddf073d0475eff697b253e012f
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-03-15 18:55:09 -0700

    feat(User): created convo field tied to MessageGroup

--- models/User.js
+++ models/User.js
@@ -2,23 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
     email: {
         type: String,
         required: true,
-        // unique: true
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
     },
     password: {
         type: String,
         required: true
     },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+    conversations: [{ type: Schema.Types.ObjectId, ref: 'MessageGroup' }],
 });
 
 module.exports = mongoose.model("User", UserSchema);
