016d00d56ecb879e3838b980a348144ab8ad5801
FredoDiaz <geekwise.alfredo.diaz@gmail.com>
2020-03-16 14:11:45 -0700

    refactor: added receiver, sender to holders

--- models/MessageGroup.js
+++ models/MessageGroup.js
@@ -3,8 +3,7 @@ const Schema = mongoose.Schema
 
 const MessageSchema = new Schema({
     messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
-    receiver: { type: Schema.Types.ObjectId, ref: 'User' },
-    sender: { type: Schema.Types.ObjectId, ref: 'User' },
+    holders: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 });
 
 module.exports = mongoose.model("MessageGroup", MessageSchema);
